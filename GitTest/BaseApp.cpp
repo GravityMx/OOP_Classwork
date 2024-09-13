a203ce56ba9cf267a9b39f74eb0ea3b1d1e769db
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-09-13 08:59:01 -0400

    This program takes 3 values and give what percentage of the total of all the numbers given is.

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+int main() {
+    double value1, value2, value3, total;
+
+    cout << "Welcome to the calcalating percentages program!\n";
+
+    cout << "Enter the first value: ";
+
+    cin >> value1;
+
+    cout << "Enter the second value: ";
+
+    cin >> value2;
+
+    cout << "Enter the third value: ";
+
+    cin >> value3;
+
+    total = value1 + value2 + value3;
+
+
+    cout << "Percentage of value1: " << (value1 / total) * 100 << "%\n";
+    cout << "Percentage of value2: " << (value2 / total) * 100 << "%\n";
+    cout << "Percentage of value3: " << (value3 / total) * 100 << "%\n";
 }
