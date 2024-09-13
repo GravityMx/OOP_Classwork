1904ba3856b4bde894bd7efa33ad8afd85efe2a8
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-09-13 13:15:58 -0400

    This progrm is finished.

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+int main() {
+    double principle, rate, time, finalSaving;
+
+    cout << "Enter the initial amount: ";
+    cin >> principle;
+
+    cout << "Enter the annual interest rate: ";
+    cin >> rate;
+
+    cout << "Enter the number of times interest is compounded per year: ";
+    cin >> time;
+
+    finalSaving = principle * pow((1 + (rate / time)), time);
+
+    cout << "The final savings at the end of the year is: " << finalSaving;
 }
