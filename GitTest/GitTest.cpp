2736e3b8d3aab6ce4f4acf129177ea3d1394d94e
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-09-13 08:37:52 -0400

    This program calculates the new price of an item at a given discount.

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -1,10 +1,23 @@
-
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    float basePrice, discount, finalPrice;
 
-//Base
+    cout << "Welcome to the discount calculation app! \n";
+
+    cout << "Please enter the base price of the item: ";
+
+    cin >> basePrice;
+
+    cout << "\nPlease enter the discount (as a decimal): ";
+
+    cin >> discount;
+
+    finalPrice = basePrice - (basePrice * discount);
+
+    cout << "\nYour total comes to : " << finalPrice;
+
+}
