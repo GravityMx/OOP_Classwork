6608f88b32ffb8cb1a6454c56a0281f9e6965eef
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-09-13 12:45:42 -0400

    The program is done.

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	float sugar = 0.03125, butter = 0.021, flour = 0.0573;
+	int cookieCount;
+
+	cout << "How many cookies would you like to bake:";
+
+	cin >> cookieCount;
+
+	cout << "\n\nYou will need...\n" << (sugar * cookieCount) << "Cups of sugar,";
+
+	cout << "\n" << (butter * cookieCount) << " cups of butter,";
+
+	cout << "\nand " << (flour * cookieCount) << " cups of flour.";
 }
