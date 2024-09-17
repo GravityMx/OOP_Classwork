98592d89252c0a21f8cfc0cb93f61df98ff52333
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-09-17 14:03:11 -0400

    It takes the two files, sorts them alphabetically, and then makes a new output file with them.

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    ifstream file1("C:/Users/kenda/Desktop/CollegeWork/ObjectOrientedProgramming/Test1.3.txt");
+    ifstream file2("C:/Users/kenda/Desktop/CollegeWork/ObjectOrientedProgramming/Test1.3_2.txt");
+    ofstream output("C:/Users/kenda/Desktop/CollegeWork/ObjectOrientedProgramming/output.txt");
+
+    vector<string> lines;
+    string line;
+
+    while (getline(file1, line))
+    {
+        lines.push_back(line);
+    }
+
+    while (getline(file2, line))
+    {
+        lines.push_back(line);
+    }
+
+    sort(lines.begin(), lines.end());
+
+    for (const auto& l : lines)
+    {
+        output << l << endl;
+    }
+
+    return 0;
 }
