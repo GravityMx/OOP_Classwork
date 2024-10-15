e1732e6d65c46a0ce8b4988b88cc2ccc3890855d
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-10-14 20:00:08 -0400

    This program demonstraits a basic counter.

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -2,7 +2,66 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+class counterType {
+private:
+    int counter;
+
+public:
+    counterType(int cont) {
+        counter = cont;
+    }
+
+    //set the counter value
+    void setCounter(int value) {
+        if (value >= 0)
+            counter = value;
+        else
+            cout << "Counter value must be nonnegative." << endl;
+    }
+
+    // retrieve the counter value
+    int getCounter() const {
+        return counter;
+    }
+
+    // increment the counter
+    void incrementCounter() {
+        counter++;
+    }
+
+    // decrement the counter
+    void decrementCounter() {
+        if (counter > 0)
+            counter--;
+        else
+            cout << "Counter cannot be negative." << endl;
+    }
+
+    //reset the counter
+    void resetCounter() {
+        counter = 0;
+    }
+};
+
+int main() {
+    counterType myCounter(0);
+
+    // Test the class functionality
+    myCounter.setCounter(10);
+    cout << "Counter value: " << myCounter.getCounter() << endl; 
+
+    myCounter.incrementCounter();
+    cout << "Counter after increment: " << myCounter.getCounter() << endl; 
+
+    myCounter.decrementCounter();
+    cout << "Counter after decrement: " << myCounter.getCounter() << endl;
+
+    myCounter.decrementCounter();
+    myCounter.decrementCounter();
+    cout << "Counter after 2 more decrements: " << myCounter.getCounter() << endl;
+
+    myCounter.resetCounter();
+    cout << "Counter after reset: " << myCounter.getCounter() << endl; // Output: 0
+
+    return 0;
 }
