49e25379ac639137876770e570b4daefa88785cb
GravityMx <98619451+GravityMx@users.noreply.github.com>
2024-11-11 22:33:10 -0500

    Just started and am getting syntax wrong

--- GitTest/BaseApp.cpp
+++ GitTest/BaseApp.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+// Employee Class
+// Represents an employee with name, number, and hire date.
+
+Class Employee
+{
+    private:
+        string employeeName
+        int employeeNumber
+        string hireDate
+
+    Public :
+        // Constructor to initialize employee details
+        Employee(string name, int number, string date)
+
+            // Accessors and Mutators
+            string getEmployeeName()
+            void setEmployeeName(string name)
+
+            int getEmployeeNumber()
+            void setEmployeeNumber(int number)
+
+            string getHireDate()
+            void setHireDate(string date)
+
+            // Function to print employee details
+            void print()
+                Print "Name: " + employeeName
+                Print "Number: " + employeeNumber
+                Print "Hire Date: " + hireDate
+};
+
 int main()
 {
     cout << "Hello World!\n";
